1c49fbb5d43b99ff52106b8cfd122b7ecc45447b
xingyan <lucky_xyic@sina.cn>
2019-07-29 17:51:58 +0800

    service 与 activity 通信

--- app/src/main/java/com/example/dragdemo/ServiceToActivity.java
+++ app/src/main/java/com/example/dragdemo/ServiceToActivity.java
@@ -8,6 +8,11 @@ import android.widget.Button;
 
 /**
  * activity 和 servise 通信的方式
+ * 方法一：扩展Binder
+ * 方法二：广播 Broadcast
+ * 方法三：共享文件 SharedPreferences 高并发不适用
+ * 方法四：Messenger 信使 一种轻量级的IPC方案，底层是用AIDL实现的
+ * 方法五：AIDL Android的IPC机制，常用于跨进程通信，主要实现原理基于底层Binder机制
  */
 public class ServiceToActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnBinder;
@@ -34,7 +39,7 @@ public class ServiceToActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_binder://通过 Binder 进行通信
-                startActivity(new Intent(this,ServiceBinderActivity.class));
+                startActivity(new Intent(this, ServiceBinderActivity.class));
                 break;
             case R.id.btn_broadcast://通过 Broadcast 广播
                 break;
